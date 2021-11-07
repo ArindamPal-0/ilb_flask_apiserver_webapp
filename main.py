476eafd2a459e66419a271d196b190c06b41bf24
Arindam Pal <arindampal2000@gmail.com>
2021-11-07 21:22:44 +0530

    created the led api
    implemented led RESTful api which gets and changes the state of LED

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask
+from flask import Flask, Blueprint, jsonify, make_response, request
 
 app = Flask(__name__)
 
@@ -41,13 +41,30 @@ def toggleLEDBlinking() -> dict:
     else:
         return dict()
 
+api = Blueprint('api', __name__, url_prefix='/api')
 
+@api.route('/led')
+def get_led_status():
+    status = getStatus()
+    response = make_response(jsonify(status), 200)
+    response.headers['content-type'] = 'application/json'
+    return response
+
+@api.route('/led', methods=['POST'])
+def set_led_status():
+    if 'ledOn' in request.json:
+        status = toggleLED()
+    if 'blinking' in request.json:
+        status = toggleLEDBlinking()
+    response = make_response(jsonify(status), 200)
+    response.headers['content-type'] = 'application/json'
+    return response
+
+app.register_blueprint(api)
 
 @app.route('/')
 def index():
     return "<h1>Hello, World!</h1>"
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=5000)
-
-    print(toggleLED())
+    app.run(debug=True, host='0.0.0.0', port=5000)
